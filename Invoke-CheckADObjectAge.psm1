a559657219a2c2e079babd6cc86c439f57633290
Jeff Roberson <jeffr@hutchgov.com>
2024-05-02 16:39:35 -0500

    Add files via upload

---
+++ Invoke-CheckADObjectAge.psm1
@@ -0,0 +1,19 @@
+﻿
+function Invoke-CheckADObjectAge ()
+{
+  param(
+    $Warning = $null,
+    $Critical = $null,
+    [switch]$NoPerfData = $FALSE,
+    [ValidateSet(0,1,2)]
+    $Verbosity = 0
+  );
+  $TestUser = Get-ADObject -Identity "CN=Nagios Test,CN=Users,DC=hutchgov,DC=com" -Properties Description;
+  $Stamp = $TestUser.Description;
+  $Age = [DateTimeOffset]::Now.ToUnixTimeSeconds() - $Stamp;
+
+  $result = New-IcingaCheck -Name "AD Object Age $([timespan]::fromseconds($Age).ToString("hh\:mm\:ss"))" -Value $Age;
+  $result.WarnOutOfRange($Warning).CritOutOfRange($Critical) | Out-Null;
+
+  return (New-IcingaCheckResult -Check $result -NoPerfData $NoPerfData -Compile);
+}
---
+++ Invoke-UpdateADObjectAge.psm1
@@ -0,0 +1,9 @@
+﻿Function Invoke-UpdateADObjectAge()
+{
+    Set-ADObject -Identity "CN=Nagios Test,CN=Users,DC=hutchgov,DC=com" -Description "$([DateTimeOffset]::Now.ToUnixTimeSeconds())";
+    $Result = New-IcingaCheck -Name "AD Object Updated" -Value 0;
+    $Result.WarnOutOfRange(1).CritOutOfRange(2) | Out-Null;
+
+    return (New-IcingaCheckResult -Check $Result -Compile);
+}
+ 
