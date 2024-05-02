f1780dca89ddb5b497684df2bc101fae09e8b1c6
Jeff Roberson <jeffr@hutchgov.com>
2024-05-02 16:45:48 -0500

    Invoke-UpdateADObjectAge.psm1

--- Invoke-UpdateADObjectAge.psm1
+++ Invoke-UpdateADObjectAge.psm1
@@ -1,6 +1,6 @@
 ﻿Function Invoke-UpdateADObjectAge()
 {
-    Set-ADObject -Identity "CN=Nagios Test,CN=Users,DC=hutchgov,DC=com" -Description "$([DateTimeOffset]::Now.ToUnixTimeSeconds())";
+    Set-ADObject -Identity "CN=TestObject,CN=Users,DC=contoso,DC=com" -Description "$([DateTimeOffset]::Now.ToUnixTimeSeconds())";
     $Result = New-IcingaCheck -Name "AD Object Updated" -Value 0;
     $Result.WarnOutOfRange(1).CritOutOfRange(2) | Out-Null;
 
